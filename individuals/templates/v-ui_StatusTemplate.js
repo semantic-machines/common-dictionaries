5c9f70db6ee6beae8c793edbc28dfd86ba1a730b
Roman Karpov <roman.karpov@gmail.com>
2022-01-08 10:18:13 +0300

    style: prettify

--- individuals/templates/v-ui_StatusTemplate.js
+++ individuals/templates/v-ui_StatusTemplate.js
@@ -4,9 +4,7 @@ export const post = function (individual, template, container, mode, extra) {
   template = $(template);
   container = $(container);
 
-  template.addClass("label-"+individual["v-s:tag"]);
+  template.addClass('label-' + individual['v-s:tag']);
 };
 
-export const html = `
-<span about="@" class="label margin-xs-h" property="rdfs:label"></span>
-`;
+export const html = ` <span about="@" class="label margin-xs-h" property="rdfs:label"></span> `;
